ebc2226de490fe0dd7bc53c8d3e7c837c64b4ac3
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-03 09:04:14 +0600

    adding pages with default styling

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-me">
+    <h1>ABout me page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum debitis ipsum quaerat rem quos nam ratione,</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum debitis ipsum quaerat rem quos nam ratione,</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum debitis ipsum quaerat rem quos nam ratione,</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum debitis ipsum quaerat rem quos nam ratione,</p>
+  </div>
+</template>
+
+<style scoped>
+.about-me {
+  text-align: center;
+  padding: 30px;
+}
+</style>
