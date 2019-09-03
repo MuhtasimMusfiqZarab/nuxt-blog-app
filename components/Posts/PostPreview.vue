bd8a610ee0526b9cd20b114dcbe247c9eef12960
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-03 10:27:51 +0600

    splitting page into components

---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,81 @@
+
+
+<template>
+  <!-- Reusing components to show post preview for individual post -->
+  <!-- to is a dynamic link -->
+  <!--image url https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260 -->
+
+  <nuxt-link :to="'/posts/' + id" class="post-preview">
+    <article>
+      <div class="post-thumbnail" :style="{backgroundImage: 'url(' + thumbnail + ')'}"></div>
+      <div class="post-content">
+        <h1>{{title}}</h1>
+        <p>{{previewText}}</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+
+<script>
+// (names only help us while debugging if error messages is generated)
+export default {
+  name: "PostPreview",
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    previewText: {
+      type: String,
+      required: true
+    },
+    thumbnail: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
