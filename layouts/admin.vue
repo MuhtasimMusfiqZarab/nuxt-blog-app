471c20592b5e562a5f0607643b9f437448813886
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-03 16:14:59 +0600

    adding a separate layout to admin

---
+++ layouts/admin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+
+
+<style>
+/* using global cdn(google font) for the whole document importing inside of config file  */
+html {
+  font-family: "Hepta Slab", serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="error-page">
+    <h1>Oops, something went wrong!</h1>
+    <!--anchor tag is used because if app clashes then nuxt link wont work anymore-->
+    <p>
+      Back to
+      <a href="/">safety</a>!
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.error-page {
+  text-align: center;
+}
+
+.error-page a {
+  text-decoration: none;
+  color: red;
+}
+
+.error-page a:hover,
+.error-page a:active {
+  color: salmon;
+}
+</style>
