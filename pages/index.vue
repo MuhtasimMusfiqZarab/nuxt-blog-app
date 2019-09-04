e1b48271412495621a93aac0a9cc3207ae4952ff
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-04 10:30:22 +0600

    adding asynData property

--- pages/index.vue
+++ pages/index.vue
@@ -14,33 +14,46 @@ export default {
   components: {
     PostList
   },
-  data() {
-    return {
-      // this need to be an array to loop through
-      loadedPosts: [
-        {
-          id: "1",
-          title: " First Post",
-          previewText: "This is the first post",
-          thumbnail:
-            "https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-        },
-        {
-          id: "2",
-          title: " Second Post",
-          previewText: "This is the second post",
-          thumbnail:
-            "https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-        },
-        {
-          id: "3",
-          title: " Third Post",
-          previewText: "This is the third post",
-          thumbnail:
-            "https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-        }
-      ]
-    };
+  // special property on the pages components (only) that nuxt will execute on the server
+  // server waits for it to finish and then return it to the client (hence we will get a pre-rendered complete page back to the client)
+  // this also replaces the data() as well as created() in the client side (this executes on the server)
+  // we cant use normal data then which will override the asyncData & can get unwanted effect
+  asyncData(context, callback) {
+    console.log("async data is loaded");
+    // this keyword does not work as expected in async data coz async data runs before this component is created
+    // we need to give asyncData the idea about when we are done (otherwise imediately returns the finished page
+    // one solution is to return a promise ( thus it waits for the promise to be resolved and gives the result)
+    // the syntax is return new Promise() which is to wrap the whole setTimeout
+    // other one is to use callback (in aync data we get two arguments (context, callback)), execute callback when we are done
+    // execution of callback method tells the async method that we are done
+    // we use callback instead of return
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: " First Post",
+            previewText: "This is the first post",
+            thumbnail:
+              "https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+          },
+          {
+            id: "2",
+            title: " Second Post",
+            previewText: "This is the second post",
+            thumbnail:
+              "https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+          },
+          {
+            id: "3",
+            title: " Third Post",
+            previewText: "This is the third post",
+            thumbnail:
+              "https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+          }
+        ]
+      });
+    }, 1500);
   }
 };
 </script>
